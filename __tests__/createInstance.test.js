6645db222a53e223fd9b9c83ff965a50b251f2ff
FJackson <jacksonfinal@gmail.com>
2024-12-08 21:48:14 -0300

    gkjh

---
+++ __tests__/createInstance.test.js
@@ -0,0 +1,55 @@
+import { describe, it, beforeEach, expect } from "vitest";
+import mongoose from "mongoose";
+import { _FKS_MODEL_ } from "../models.js";
+import { InitMongoModels, MongoModel } from "../mongoClass.js";
+
+const connectMongoDb = async function connect(url) {
+    const mongoOptions = {
+        serverSelectionTimeoutMS: 5000,
+    };
+
+    return await mongoose.connect(url, mongoOptions);
+};
+
+describe("Mongo instance creation", () => {
+    const testSchema = new mongoose.Schema({
+        name: { type: String, required: true },
+        related: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "RelatedModel",
+            __linked: true,
+            required: true,
+        },
+    });
+
+    const relatedSchema = new mongoose.Schema({
+        title: { type: String, required: true },
+    });
+
+    const syncedModels = InitMongoModels();
+
+    beforeEach(async () => {
+        await connectMongoDb("mongodb+srv://jacksonjfs18:eUAqgrGoVxd5vboT@cluster0.o5i8utp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+
+        const collections = await mongoose.connection.db.listCollections().toArray();
+        const dropPromises = collections.map((collection) =>
+            mongoose.connection.db.dropCollection(collection.name)
+        );
+
+        await Promise.all(dropPromises);
+
+        await syncedModels.set([]);
+
+        for (let model in mongoose.models) {
+            delete mongoose.models[model];
+        }
+    });
+
+    afterEach(async () => {
+        await mongoose.connection.close();
+    });
+
+    it("", async () => {
+
+    });
+});
