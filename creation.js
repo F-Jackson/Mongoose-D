b57f87023bd04266700a39e5bd3546f2987fedf1
FJackson <jacksonfinal@gmail.com>
2024-12-08 21:46:12 -0300

    hgjhg

--- creation.js
+++ creation.js
@@ -1,20 +1,22 @@
 import { _FKS_ } from "./models.js";
 
-
 export class ForeignKeyCreator {
     constructor(mongoModel) {
         this.mongoModel = mongoModel;
         this.modelName = this.mongoModel.modelName;
         this.fksModels = Object.entries(this.mongoModel.__FKS__);
-        this.relations = [];
-        this.promisesCreateFks = [];
     }
 
     async _creationFks(instance) {
+        const relations = [];
+        const promisesCreateFks = [];
+
         try {
-            await this._createFks(instance);
+            await this._createFks(instance, relations, promisesCreateFks);
+            await Promise.all(promisesCreateFks);
+            return relations;
         } catch (error) {
-            await this._cleanupRelations();
+            await this._cleanupRelations(relations);
             throw error;
         }
     }
@@ -23,21 +25,22 @@ export class ForeignKeyCreator {
         if (!Array.isArray(models)) models = [models];
 
         try {
-            models.map(async model => {
+            const allRelations = [];
+
+            for (const model of models) {
                 const relations = await this._creationFks(model);
                 model.__relations = relations;
-            });
-            
-            await Promise.all(promisesCreateFks);
+                allRelations.push(...relations);
+            }
 
-            await this._bulkInsertRelations();
+            await this._bulkInsertRelations(allRelations);
         } catch (error) {
             await this._cleanupModels(models);
             throw error;
         }
     }
 
-    async _createFks(instance) {
+    async _createFks(instance, relations, promisesCreateFks) {
         const modelId = instance._id;
 
         for (const [key, value] of this.fksModels) {
@@ -49,9 +52,9 @@ export class ForeignKeyCreator {
             const uniqueCheckPromises = ids.map(id => this._ensureUnique(value, id, modelId));
             await Promise.all(uniqueCheckPromises);
 
-            this.promisesCreateFks.push(
-                ...ids.map(id => this._createRelation(modelId, id, value, relations))
-            );
+            ids.forEach(id => {
+                promisesCreateFks.push(this._createRelation(modelId, id, value, relations));
+            });
         }
     }
 
@@ -61,39 +64,41 @@ export class ForeignKeyCreator {
             parent_id: modelId,
             child_ref: fk.ref,
             child_id: id,
-            child_fullPath: fk.fullPath
+            child_fullPath: fk.fullPath,
         }).lean();
 
         if (existingRelation) throw new Error("Relation already exists");
     }
 
-    async _createRelation(modelId, childId, value) {
+    async _createRelation(modelId, childId, value, relations) {
         const fkRelation = {
             parent_id: modelId,
             parent_ref: this.modelName,
             child_id: childId,
             child_ref: value.ref,
-            child_fullPath: value.fullPath
+            child_fullPath: value.fullPath,
         };
 
         relations.push(fkRelation);
     }
 
-    async _cleanupRelations() {
-        if (this.relations.length === 0) return;
-        const relationIds = this.relations.map(relation => relation._id);
-        await _FKS_.deleteMany({ _id: { $in: relationIds } });
+    async _cleanupRelations(relations) {
+        if (relations.length > 0) {
+            const relationIds = relations.map(relation => relation._id);
+            await _FKS_.deleteMany({ _id: { $in: relationIds } });
+        }
     }
 
     async _cleanupModels(models) {
-        if (models.length === 0) return;
-        const modelIds = models.map(model => model._id);
-        await this.mongoModel.deleteMany({ _id: { $in: modelIds } });
+        if (models.length > 0) {
+            const modelIds = models.map(model => model._id);
+            await this.mongoModel.deleteMany({ _id: { $in: modelIds } });
+        }
     }
 
-    async _bulkInsertRelations() {
-        if (this.relations.length > 0) {
-            await _FKS_.insertMany(this.relations);
+    async _bulkInsertRelations(relations) {
+        if (relations.length > 0) {
+            await _FKS_.insertMany(relations);
         }
     }
 }
