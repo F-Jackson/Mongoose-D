94eb2c28c8ecccbdc37578da1b919bd48fad854b
FJackson <jacksonfinal@gmail.com>
2024-12-14 16:47:26 -0300

    hgj

--- models.js
+++ models.js
@@ -1,82 +1,10 @@
 import { Schema } from "mongoose";
 import mongoose from "mongoose";
 
-
-const __FKS__ = new Schema({
-    parent_id: {
-        type: String,
-        required: true,
-        minLength: 0,
-    },
-    parent_ref: {
-        type: String,
-        required: true,
-        immutable: true,
-        minLength: 0
-    },
-    child_id: {
-        type: String,
-        required: true,
-        immutable: true
-    },
-    child_ref: {
-        type: String,
-        required: true,
-        immutable: true,
-        minLength: 0
-    },
-    child_fullPath: {
-        type: String,
-        required: true,
-        immutable: true,
-        minLength: 0
-    }
+export const RelationSchema = new Schema({
+    to_id: mongoose.Schema.Types.ObjectId,
+    from_id: mongoose.Schema.Types.ObjectId,
+    toClass: String,
+    fromClass: String,
+    path: String,
 });
-
-const __FKS__MODEL__ = new Schema({
-    model: {
-        type: String,
-        required: true,
-        immutable: true,
-        minLength: 0,
-    },
-    fk: {
-        type: String,
-        required: true,
-        immutable: true,
-        minLength: 0
-    },
-    fk_ref: {
-        type: String,
-        required: true,
-        immutable: true,
-        minLength: 0
-    },
-    fk_isArray: {
-        type: Boolean,
-        required: true,
-        immutable: true,
-        default: false
-    },
-    fk_isImmutable: {
-        type: Boolean,
-        required: true,
-        immutable: true,
-        default: false
-    },
-    fk_isRequired: {
-        type: Boolean,
-        required: true,
-        immutable: true,
-        default: false
-    },
-    fk_isUnique: {
-        type: Boolean,
-        required: true,
-        immutable: true,
-        default: false
-    }
-});
-
-export const _FKS_ = mongoose.model("__FKS__", __FKS__);
-export const _FKS_MODEL_ = mongoose.model("__FKS__MODEL__", __FKS__MODEL__);
